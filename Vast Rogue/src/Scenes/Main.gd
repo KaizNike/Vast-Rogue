a399782f9c3cd37beb5cba4d9238f219d399ba44
KaizNike <62921588+KaizNike@users.noreply.github.com>
2021-10-26 20:00:38 -0500

    Enter button valid command coloring
    - Added version info (v2.0.1-alpha)

--- Vast Rogue/src/Scenes/Main.gd
+++ Vast Rogue/src/Scenes/Main.gd
@@ -1,6 +1,7 @@
 extends Node
 
 onready var textEdit = $UI/VBoxContainer/TextEdit
+var version = [2, 0, 1, "-alpha"]
 
 var Things = {
 	"#": {"collidable": true},
@@ -65,18 +66,21 @@ var lookPrompts = [
 var player = {"loc": Vector2(1,1), "floor": 0, "currentRoom": "playerRoom", "hp": 0}
 
 func _ready():
+	randomize()
 	textEdit.text = ""
 #	print(Rooms[1])
 #	print(player.loc)
-	var f = randi() % 2 - 1
+	var f = randi() % 2
 	print(f)
 	var text = storyPrompts[0] + "\n" + healthPrompts[f] + "\n\n" + hintPrompts[0]
 	display_text_by_scrolling(text)
 #	command_parse("look")
 
+# Takes string from line edit text entered and tries to perform the desired command
 func command_parse(text : String):
 	if text == "":
 		return
+#	split the text to find subqueries
 	var parse : PoolStringArray = text.split(" ")
 	if text == "look" or text == "look around":
 		var printText= ""
@@ -92,6 +96,8 @@ func command_parse(text : String):
 		textEdit.text = ""
 		display_text_by_scrolling(printText)
 		pass
+	elif text == "version":
+		display_text_by_scrolling("V " + str(version[0]) + "." + str(version[1]) + "." + str(version[2]) + version[3])
 	elif parse.size() == 3:
 		if parse[0] == "look" and parse[1] == "at":
 			if parse[2] == "keypad":
@@ -132,6 +138,12 @@ func check_collision(dir, special) -> bool:
 func process_turn():
 	pass
 
+func switch_green(Bool: bool):
+	if Bool:
+		$UI/VBoxContainer/HSplitContainer/Button.modulate = ColorN("green", 1)
+	else:
+		$UI/VBoxContainer/HSplitContainer/Button.modulate = ColorN("white", 1)
+
 func _on_Timer_timeout():
 	if textStore:
 		if textStoreIndex < textStoreLen:
@@ -143,6 +155,33 @@ func _on_Timer_timeout():
 		pass # Replace with function body.
 
 
+# Done after pressing enter or button.
 func _on_LineEdit_text_entered(new_text):
 	$UI/VBoxContainer/HSplitContainer/LineEdit.text = ""
 	command_parse(new_text)
+
+
+# To be updated after command parse is.
+# Update the screen sometimes after text is entered
+func _on_LineEdit_text_changed(new_text):
+	var parse : PoolStringArray = new_text.split(" ")
+	var size = parse.size()
+	if new_text == "look" or new_text == "look around":
+		switch_green(true)
+		pass
+	elif new_text == "version":
+		switch_green(true)
+	elif parse[0] == "look" and parse[1] == "at" and size == 3: 
+		if parse[2] == "keypad":
+			switch_green(true)
+			pass
+		else:
+			switch_green(false)
+	else:
+		switch_green(false)
+
+
+# When ==> pressed, doubles as enter
+func _on_Button_pressed():
+	_on_LineEdit_text_entered($UI/VBoxContainer/HSplitContainer/LineEdit.text)
+	switch_green(false)
